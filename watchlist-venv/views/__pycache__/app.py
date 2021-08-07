5b6bb882133e6e2516885c75f1f1deb2d9f98363
shiyi <albertzzzsy@gmail.com>
2021-08-07 10:20:27 +0200

    ~

--- watchlist-venv/views/__pycache__/app.py
+++ watchlist-venv/views/__pycache__/app.py
@@ -6,6 +6,10 @@ import sys
 import click
 from flask import request
 from flask import flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
+from flask_login import login_user
+from flask_login import login_required, logout_user
 
 app = Flask(
     __name__,
@@ -67,34 +71,64 @@ def index():
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
 
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return 'User: %s' % escape(name)
-# @app.route('/test')
-# def test_url_for():
-#     # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-#     print(url_for('hello'))  # 输出：/
-#     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-#     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-#     print(url_for('user_page', name='peter'))  # 输出：/user/peter
-#     print(url_for('test_url_for'))  # 输出：/test
-#     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-#     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-#     return 'Test page'
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
 
-# @app.cli.command()  # 注册为命令
-# @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
-# def initdb(drop):
-#     """Initialize the database."""
-#     if drop:  # 判断是否输入了选项
-#         db.drop_all()
-#     db.create_all()
-#     click.echo('Initialized database.')  # 输出提示信息
 
 @app.context_processor
 def inject_user():  # 函数名可以随意修改
     user = User.query.first()
     return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+login_manager = LoginManager(app)  # 实例化扩展类
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
+
+@app.cli.command()  # 注册为命令
+# @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)  # 设置密码
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)  # 设置密码
+        db.session.add(user)
+
+    db.session.commit()  # 提交数据库会话
+    click.echo('Done.')
+    
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 @app.cli.command()
 def forge():
@@ -128,6 +162,14 @@ def forge():
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128))  # 密码散列值
+
+    def set_password(self, password):  # 用来设置密码的方法，接受密码作为参数
+        self.password_hash = generate_password_hash(password)  # 将生成的密码保持到对应字段
+
+    def validate_password(self, password):  # 用于验证密码的方法，接受密码作为参数
+        return check_password_hash(self.password_hash, password)  # 返回布尔值
 
 class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
@@ -166,3 +208,32 @@ def delete(movie_id):
     db.session.commit()  # 提交数据库会话
     flash('Item deleted.')
     return redirect(url_for('index'))  # 重定向回主页
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        # 验证用户名和密码是否一致
+        if username == user.username and user.validate_password(password):
+            login_user(user)  # 登入用户
+            flash('Login success.')
+            return redirect(url_for('index'))  # 重定向到主页
+
+        flash('Invalid username or password.')  # 如果验证失败，显示错误消息
+        return redirect(url_for('login'))  # 重定向回登录页面
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required  # 用于视图保护，后面会详细介绍
+def logout():
+    logout_user()  # 登出用户
+    flash('Goodbye.')
+    return redirect(url_for('index'))  # 重定向回首页
